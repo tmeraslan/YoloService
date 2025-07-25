fac46bbb6eed7500c0e02139c93de5db9d2cb8a4
tamer <tmeraslan1@gmail.com>
2025-07-25 18:09:39 +0300

    fix_conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
+# tests/conftest.py
 import os
 import pytest
+from db import engine, Base
 
 @pytest.fixture(scope="session", autouse=True)
 def create_upload_dirs():
     os.makedirs("uploads/original", exist_ok=True)
     os.makedirs("uploads/predicted", exist_ok=True)
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_tables():
+    Base.metadata.create_all(bind=engine)
