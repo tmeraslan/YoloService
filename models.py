92d68b153a4cfe29c08454d6648cf72093d6227b
tamer <tmeraslan1@gmail.com>
2025-07-25 18:43:44 +0300

    fix

--- models.py
+++ models.py
@@ -4,10 +4,11 @@ from sqlalchemy import Column, String, DateTime, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from sqlalchemy.orm import declarative_base
-
+from db import  Base
 
 # All models inherit from this base class
-Base = declarative_base()
+# Base = declarative_base()
+
   
 
 class User(Base):
