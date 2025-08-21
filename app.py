3b06cc57ce96bf6c5c9c7b099ccab1d8bec23628
tamer <tmeraslan1@gmail.com>
2025-08-21 11:09:31 +0300

    allure

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ app.include_router(controllers.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
